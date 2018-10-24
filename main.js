6ff3be74923378e1512f0f8567d1f8db09c7694f
solzik <Sdebre10@gmail.com>
2018-10-24 06:14:57 -0400

    adding html,css and js file

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict'
+let Aboutme =  prompt('do u think I am  patience?')
+if(Aboutme.toLowerCase() === 'yes'|| Aboutme.toUpperCase() === 'YES')
+{
+    alart('u are patience')
+
+}else{
+    Aboutme.toLowerCase() === 'no'|| Aboutme.toUpperCase() === 'NO'
+}
+
+let fun = prompt('do u fear mouse?')
+console.log('I fear Mouse', yes)
+if(fun.toLowerCase() === 'yes'|| Aboutme.toUpperCase() === 'YES'){
+}
+else{(fun.toLowerCase() === 'no'|| Aboutme.toUpperCase() === 'NO')
+
+}
+let subject = prompt('did u kiss before the age of 14?')
+if(subject.toLowerCase() === 'yes'|| subject.toUpperCase() === 'YES'){
+}
+else{(subject.toLowerCase() === 'no'|| subject.toUpperCase() === 'NO')
+
+}
+let gear = prompt('did u talk bad word that makes people angry?')
+if(gear.toLowerCase() === 'yes'|| gear.toUpperCase() === 'YES'){
+}
+else{
+    (gear.toLowerCase() === 'no'|| gear.toUpperCase() === 'NO')}
+let join = prompt('did u lie?')
+if(join.toLowerCase() === 'yes'|| join.toUpperCase() === 'YES'){
+}
+else{
+    (join.toLowerCase() === 'no'|| join.toUpperCase() === 'NO')
+}
+
