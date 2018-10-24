60e8b1841b38b6832d33db0ef2911f2753545e6c
solzik <Sdebre10@gmail.com>
2018-10-24 07:32:39 -0400

    i want to change my js

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 'use strict'
 let Aboutme =  prompt('do u think I am  patience?')
-if(Aboutme.toLowerCase() === 'yes'|| Aboutme.toUpperCase() === 'YES')
+if(Aboutme == "yes"|| Aboutme == "YES")
 {
-    alart('u are patience')
-
-}else{
-    Aboutme.toLowerCase() === 'no'|| Aboutme.toUpperCase() === 'NO'
+    alert('yap am patience')
+}
+else if(Aboutme == "no"|| Aboutme == "NO")
+{
+alert("i don't thick so")
 }
 
 let fun = prompt('do u fear mouse?')
-console.log('I fear Mouse', yes)
-if(fun.toLowerCase() === 'yes'|| Aboutme.toUpperCase() === 'YES'){
+console.log(fun)
+if(fun == "yes"|| fun == "YES"){
+    alert("ohh i also fear mouse");
 }
-else{(fun.toLowerCase() === 'no'|| Aboutme.toUpperCase() === 'NO')
+else if (fun== "no"|| fun === "NO"){
+alert("you are like me")
+}
 
-}
 let subject = prompt('did u kiss before the age of 14?')
-if(subject.toLowerCase() === 'yes'|| subject.toUpperCase() === 'YES'){
+console.log(subject);
+if(subject == "yes"|| subject == "YES"){
+    alert("OMG ....good job men")
 }
-else{(subject.toLowerCase() === 'no'|| subject.toUpperCase() === 'NO')
 
+else if(subject.toLowerCase() === 'no'|| subject.toUpperCase() === 'NO')
+{
+    alert("yap good")
 }
+
+
 let gear = prompt('did u talk bad word that makes people angry?')
-if(gear.toLowerCase() === 'yes'|| gear.toUpperCase() === 'YES'){
+
+if(gear== "yes"|| gear== "YES"){
+    alert("take it easy")
 }
-else{
-    (gear.toLowerCase() === 'no'|| gear.toUpperCase() === 'NO')}
+else if (gear== "no"|| gear== "NO"){
+    alert("fdhfd")
+}
 let join = prompt('did u lie?')
-if(join.toLowerCase() === 'yes'|| join.toUpperCase() === 'YES'){
+if(join == "yes"|| join == "YES"){
+    alert("take it easy")
 }
-else{
-    (join.toLowerCase() === 'no'|| join.toUpperCase() === 'NO')
+else if (join== "no"|| join == "NO"){
+alert("good job")
 }
+
 
